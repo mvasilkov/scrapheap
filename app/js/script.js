fdec30fab9d21f2301a729210e842097d0b2fd5e
Mark Vasilkov <mvasilkov@gmail.com>
2012-12-10 21:02:27 +0200

    Kill jQuery with fire

--- app/js/script.js
+++ app/js/script.js
@@ -1,23 +1,25 @@
-var initClock = function(block) {
-	var numbers = [];
-	
-	block.find('.display').each(function() {
-		numbers.push($(this));
-	});
+function replaceText(obj, text) {
+	while (obj.firstChild) obj.removeChild(obj.firstChild)
+	obj.appendChild(document.createTextNode(text))
+	obj.setAttribute("data-text", text)
+}
+
+(function() {
+	var numbers = document.getElementsByClassName("display");
 	
 	var formatNumber = function(n) {
 		return n < 10 ? '0' + n : '' + n;
 	};
 	
 	var updateNumber = function(block, newNumber) {
-		var currNumber = block.text();
-		block.text(newNumber);
+		var currNumber = block.getAttribute("data-text")
+		replaceText(block, newNumber)
 		
 		// Animate only if the number has changed and hasn't been empty.
-		if (newNumber !== currNumber && currNumber !== '') {
-			block.addClass("change")
-			block[0].clientHeight
-			block.removeClass("change")
+		if (newNumber !== currNumber && currNumber !== null) {
+			block.className += " change"
+			block.clientHeight
+			block.className = block.className.replace(" change", "")
 		}
 	};
 	
@@ -30,6 +32,4 @@ var initClock = function(block) {
 	
 	updateNumbers();
 	setInterval(updateNumbers, 1000);
-};
-
-initClock($('#time'));
+})()
