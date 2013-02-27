cff56f3e7e91516d699635684b79b1eeed8c68cb
Mark Vasilkov <mvasilkov@gmail.com>
2013-02-27 23:50:36 +0200

    Main loop

--- RequireJS/Build/main.js
+++ RequireJS/Build/main.js
@@ -1,11 +1,25 @@
-define(["box2d-html5"], function(box2d) {
+define(["box2d-html5", "requestAnimationFrame"], function(box2d, requestAnimationFrame) {
     var width = 960, height = 540, scale = 20,
         real_width = width / scale, real_height = height / scale,
-        canvas = null, physics = null
+        solver_velocity = 8, solver_position = 3,
+        canvas = null, physics = null, last_update = 0
+
+    function render() {
+        requestAnimationFrame(render)
+
+        var now = Date.now(), upd = 0.01 * (now - last_update)
+        last_update = now
+
+        physics.Step(upd, solver_velocity, solver_position)
+    }
 
     function main() {
         canvas = document.getElementsByTagName("canvas")[0].getContext("2d")
         physics = new box2d.b2World(new box2d.b2Vec2(0, 0.25))
+
+        last_update = Date.now()
+
+        requestAnimationFrame(render)
     }
 
     return main
