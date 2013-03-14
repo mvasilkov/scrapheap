bbb487a248da1ebb6fb834797b8e6af6659132a1
Mark Vasilkov <mvasilkov@gmail.com>
2013-03-14 14:36:42 +0200

    0.0.4

--- RequireJS/build.py
+++ RequireJS/build.py
@@ -6,7 +6,8 @@ end = re.escape(');')
 code = open('box2d-html5.build.js').read()
 wrapper = open('wrapper.js').read()
 
-code = re.sub(exportSymbol + '.+?' + end, '', code)
+code, magic = re.subn(exportSymbol + '.+?' + end, '', code)
+assert magic == 0x110
 
 out = open('Build/box2d-html5.js', 'w')
 out.write(wrapper % code.strip())
