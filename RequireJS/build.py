1e61e2e0703a6914dc0503596d88daadd7da6163
Mark Vasilkov <mvasilkov@gmail.com>
2013-03-14 14:24:13 +0200

    Don't pollute the global scope

--- RequireJS/build.py
+++ RequireJS/build.py
@@ -1,6 +1,13 @@
-code = open("box2d-html5.build.js").read()
-wrapper = open("wrapper.js").read()
+import re
 
-out = open("Build/box2d-html5.js", "w")
+exportSymbol = re.escape('goog.exportSymbol("box2d.')
+end = re.escape(');')
+
+code = open('box2d-html5.build.js').read()
+wrapper = open('wrapper.js').read()
+
+code = re.sub(exportSymbol + '.+?' + end, '', code)
+
+out = open('Build/box2d-html5.js', 'w')
 out.write(wrapper % code.strip())
 out.close()
