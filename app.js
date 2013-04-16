f875ed1cddd15440288c37dec81b07113d1fba0b
Mark Vasilkov <mvasilkov@gmail.com>
2013-04-16 19:18:12 +0300

    Drag and drop

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 var express = require("express"),
     app = express()
 
+app.use(express.bodyParser())
 app.use(express.static("public"))
 
-app.get("/hello.txt", function (req, res) {
+app.post("/~", function (req, res) {
     var body = "hello, world"
     res.setHeader("Content-Type", "text/plain")
     res.setHeader("Content-Length", body.length)
     res.end(body)
+
+    console.log("%j", req.files.image)
 })
 
 app.listen(3000)
---
+++ public/media/rarjpeg.js
@@ -0,0 +1,26 @@
+(function () {
+    var one = Array.prototype.shift.call.bind(Array.prototype.shift),
+        canvas = one(document.getElementsByTagName("canvas"))
+
+    function onDragOver(event) {
+        event.preventDefault()
+    }
+
+    function onDrop(event) {
+        var file = one(event.dataTransfer.files)
+
+        if (file) {
+            var formData = new FormData
+            formData.append("image", file)
+
+            var req = new XMLHttpRequest
+            req.open("post", "/~")
+            req.send(formData)
+        }
+
+        event.preventDefault()
+    }
+
+    canvas.addEventListener("dragover", onDragOver, false)
+    canvas.addEventListener("drop", onDrop, false)
+})()
