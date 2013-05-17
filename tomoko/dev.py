913012c4f3791b0cb71b78656903b315979da30a
Mark Vasilkov <mvasilkov@gmail.com>
2013-05-17 22:33:08 +0300

    settings.DATABASES

--- tomoko/dev.py
+++ tomoko/dev.py
@@ -1,3 +1,14 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "mokocchi",
+        "USER": "tomoko",
+        "PASSWORD": "bakabaka",
+        "OPTIONS": {
+            "unix_socket": "/tmp/mysql-dev.sock",
+        }
+    }
+}
 DEBUG = True
 ROOT_URLCONF = "tomoko.routes"
 SECRET_KEY = "Do you think I'm cute?"
