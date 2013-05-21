8cb93b02042b2964853f9c2e046ba6f1409b75f3
Mark Vasilkov <mvasilkov@gmail.com>
2013-05-21 22:58:26 +0300

    Mipmap

--- tomoko/markov/__init__.py
+++ tomoko/markov/__init__.py
@@ -2,3 +2,7 @@ from PIL import Image
 
 def break_bits(im, n):
     return Image.eval(im, lambda c: c & 256 - 2 ** n)
+
+class Mipmap:
+    def __init__(self, im, n_levels):
+        self.levels = [break_bits(im, n) for n in xrange(n_levels)]
--- tomoko/markov/test.py
+++ tomoko/markov/test.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from django.test import TestCase
-from tomoko.markov import break_bits
+from tomoko.markov import break_bits, Mipmap
 
 class MarkovTest(TestCase):
     def test_break_bits(self):
@@ -9,3 +9,9 @@ class MarkovTest(TestCase):
         im2 = break_bits(im, 2)
         self.assertEqual(im1.getpixel((0, 0)), (254, 254, 254))
         self.assertEqual(im2.getpixel((0, 0)), (252, 252, 252))
+
+    def test_mipmap(self):
+        im = Image.new("RGB", (1, 1), (127, 0, 9))
+        mm = Mipmap(im, n_levels=4)
+        self.assertEqual(mm.levels[0].getpixel((0, 0)), (127, 0, 9))
+        self.assertEqual(mm.levels[2].getpixel((0, 0)), (124, 0, 8))
