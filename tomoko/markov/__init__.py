d47f5fee7cf550de4f5676c6acfca72e73d91591
Mark Vasilkov <mvasilkov@gmail.com>
2013-06-08 15:07:29 +0300

    pixel_to_int

--- tomoko/markov/__init__.py
+++ tomoko/markov/__init__.py
@@ -1,8 +1,12 @@
 from PIL import Image
 
+def pixel_to_int(raw):
+    return raw[0] << 16 | raw[1] << 8 | raw[2]
+
 def break_bits(im, n):
     return Image.eval(im, lambda c: c & 256 - 2 ** n)
 
 class Mipmap:
     def __init__(self, im, n_levels):
         self.levels = [break_bits(im, n) for n in xrange(n_levels)]
+        self.n_levels = n_levels
