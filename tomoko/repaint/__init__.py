d3bd721ffc2f8d0d7f1e481e2c28e8bd3915ebb3
Mark Vasilkov <mvasilkov@gmail.com>
2013-06-13 18:42:50 +0300

    Mipmap.cons

--- tomoko/repaint/__init__.py
+++ tomoko/repaint/__init__.py
@@ -15,3 +15,20 @@ class Mipmap:
     def __init__(self, im, n_levels):
         self.levels = [break_bits(im, n) for n in xrange(n_levels)]
         self.n_levels = n_levels
+
+    def cons(self, start_u, start_v):
+        end = self.n_levels - 1
+        start_u -= end
+        start_v -= end
+
+        for v in xrange(self.n_levels):
+            for u in xrange(self.n_levels):
+                if u == end and v == end:
+                    return
+                uu = u + start_u
+                vv = v + start_v
+                if uu | vv < 0:
+                    yield None
+                else:
+                    level = max(end - u, end - v)
+                    yield self.levels[level].getpixel((uu, vv))
