25f4bf16acb782f229eabef9cd8868568b43f771
Mark Vasilkov <mvasilkov@gmail.com>
2013-06-14 18:12:54 +0300

    reiterate-2

--- tomoko/repaint/management/commands/repaint_save.py
+++ tomoko/repaint/management/commands/repaint_save.py
@@ -3,6 +3,7 @@ from optparse import make_option
 from tomoko.repaint import Canvas, int_to_pixel
 from tomoko.repaint.management.commands.repaint_load import MM_LEVEL
 from tomoko.repaint.models import Point
+from tomoko.repaint.reiterate import reiterate
 from tomoko.repaint.utils import progress_bar
 
 MM_SIZE = 250
@@ -16,17 +17,16 @@ class Command(LabelCommand):
         canvas = Canvas(size=options["size"], n_levels=MM_LEVEL)
         p_end = options["size"] ** 2
         p_val = 0
-        for v in xrange(options["size"]):
-            for u in xrange(options["size"]):
-                cons = tuple(canvas.cons(u, v))
-                point = Point.random_by_cons(cons)
-                if point is None:
-                    canvas.save(label)
-                    print "Eat flaming death"
-                    return
-                canvas.paint(u, v, int_to_pixel(point.value))
-                p_val += 1
-                if p_val % 4000 == 0:
-                    canvas.save(label)
-                    print progress_bar(p_val, p_end)
+        for u, v in reiterate(options["size"]):
+            cons = tuple(canvas.cons(u, v))
+            point = Point.random_by_cons(cons)
+            if point is None:
+                canvas.save(label)
+                print "Eat flaming death"
+                return
+            canvas.paint(u, v, int_to_pixel(point.value))
+            p_val += 1
+            if p_val % 4000 == 0:
+                canvas.save(label)
+                print progress_bar(p_val, p_end)
         canvas.save(label)
