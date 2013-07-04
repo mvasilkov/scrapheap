7d1ab64851f29c077365a836fc4fc4a5642d527b
Mark Vasilkov <mvasilkov@gmail.com>
2013-07-04 16:48:55 +0300

    requestAnimationFrame

---
+++ util/requestAnimationFrame.js
@@ -0,0 +1,16 @@
+if (typeof requestAnimationFrame === "undefined") {
+    ["moz", "webkit", "ms"].some(function (p) {
+        var fun = this[p + "RequestAnimationFrame"]
+        if (typeof fun === "function") {
+            return requestAnimationFrame = fun
+        }
+    }) || (requestAnimationFrame = function (fun) {
+        return setTimeout(fun, 0)
+    })
+}
+
+define(function () {
+    "use strict"
+
+    return requestAnimationFrame
+})
