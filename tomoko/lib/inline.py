2cecf475fa0e83bdfcf4bb200dcc1a5f66fd0803
Mark Vasilkov <mvasilkov@gmail.com>
2013-07-06 15:34:47 +0300

    inline

---
+++ tomoko/lib/inline.py
@@ -0,0 +1,16 @@
+from PIL import Image
+from cStringIO import StringIO
+
+def inline(image, size):
+    tmp = Image.new("RGB", (size, size), None)
+    buf = tmp.load()
+    for v in xrange(size):
+        for u in xrange(size):
+            buf[u, v] = next(image)
+
+    out = StringIO()
+    tmp.save(out, "PNG")
+    result = out.getvalue().encode("base64")
+    out.close()
+
+    return result
---
+++ tomoko/repaint/admin.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.contrib import admin
+from tomoko.repaint.models import Point
+from tomoko.lib.inline import inline
+
+class PointAdmin(admin.ModelAdmin):
+    list_display = ('__unicode__', 'visual', 'is_basic')
+
+    def visual(self, obj):
+        size = 4 * settings.RE_LEVEL
+        return ("<img src='data:image/png;base64,%s' width=%d height=%d>" %
+            (inline(obj.loop(), settings.RE_LEVEL), size, size))
+
+    visual.allow_tags = True
+
+admin.site.register(Point, PointAdmin)
