0fbda84c264823363eb87779e761a19a1fac852e
Mark Vasilkov <mvasilkov@live.com>
2016-05-18 00:04:03 +0300

    Basic URL shortener app

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from django.contrib import admin
+from shortener import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.submit, name='submit'),
+    url(r'^success/(?P<short_url>[\w-]+)$', views.success, name='success'),
+    url(r'^(?P<short_url>[\w-]+)$', views.redirect_view, name='redirect'),
 ]
