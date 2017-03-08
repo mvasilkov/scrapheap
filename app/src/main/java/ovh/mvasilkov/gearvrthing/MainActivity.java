7e496954a252283427a72f372bbbaa39fc8e82da
Mark Vasilkov <mvasilkov@gmail.com>
2017-03-08 11:51:14 +0200

    Initial commit

---
+++ app/src/main/java/ovh/mvasilkov/gearvrthing/MainActivity.java
@@ -0,0 +1,11 @@
+package ovh.mvasilkov.gearvrthing;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
