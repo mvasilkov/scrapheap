acc2e4b80086ada979397f3331970e5ca6e5848c
Mark Vasilkov <mvasilkov@gmail.com>
2017-10-29 00:04:53 +0300

    Initial commit

---
+++ diskarray_ui/settings/__init__.py
@@ -0,0 +1,9 @@
+'Debug settings'
+
+from .base import *
+
+DEBUG = True
+
+SECRET_KEY = 'Innocence proves nothing.'
+
+ALLOWED_HOSTS = ['*']
---
+++ diskarray_ui/settings/base.py
@@ -0,0 +1,84 @@
+from pathlib import Path
+
+OUR_ROOT = Path(__file__).parents[2].resolve()
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'diskarray_ui.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'diskarray_ui.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': (OUR_ROOT / 'db.sqlite3').as_posix(),
+    }
+}
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/dev/topics/i18n/
+
+LANGUAGE_CODE = 'ru-RU'
+
+TIME_ZONE = 'Israel'
+
+USE_I18N = USE_L10N = USE_TZ = False
+
+
+# Static files
+# https://docs.djangoproject.com/en/dev/howto/static-files/
+
+STATIC_ROOT = (OUR_ROOT / 'static').as_posix()
+
+STATIC_URL = '/static/'
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'yarn.finders.YarnFinder',
+]
+
+YARN_ROOT_PATH = OUR_ROOT.as_posix()
+
+YARN_STATIC_FILES_PREFIX = 'node_modules'
