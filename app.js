ab5e95e55e0d62fbd75a782849087b7fed8768ce
Mark Vasilkov <mvasilkov@gmail.com>
2018-02-14 17:19:43 +0200

    Use reCAPTCHA

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
+const toml = require('toml')
+
+const options = toml.parse(fs.readFileSync(__dirname + '/options.toml', { encoding: 'utf8' }))
+
 const express = require('express')
 const app = express()
 
-app.use('/registration/static', express.static('static'))
+const recaptcha = new (require('express-recaptcha'))(options.recaptcha.site_key, options.recaptcha.secret_key)
+
+app.use('/registration/static', express.static(__dirname + '/static', { index: false }))
+
+app.get('/registration', /* recaptcha.middleware.render, */ function (req, res) {
+    res.sendFile(__dirname + '/static/index.html')
+})
+
+app.get('/registration/done', function (req, res) {
+    res.sendFile(__dirname + '/static/done.html')
+})
+
+app.use('/registration', express.json())
+app.use('/registration', express.urlencoded({ extended: true }))
+
+app.post('/registration', recaptcha.middleware.verify, function (req, res) {
+    const { email } = req.body
+    if (email && req.recaptcha.error === null) {
+        console.log('*', email)
+        res.redirect('/registration/done')
+        return
+    }
+    res.redirect('/registration')
+})
 
 app.get('/', function (req, res) {
+    res.type('txt')
     res.send('Ghost registration app listening on 127.0.0.1:3000')
 })
 
