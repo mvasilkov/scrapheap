752a10f1d5c019a647056450e2b87db2a63fc361
Andrey Shuster <novus42@gmail.com>
2018-04-24 15:54:57 +0300

    adding post

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
+import React from 'react';
+import axios from 'axios';
 import '../styles/style.scss'
 import marked from 'marked';
 
-export default () => {
-  const text = '# Marked in the browser\n\nRendered by **marked**.';
-  return (
-    <div className="container">
-      <nav>some links</nav>
-      <header></header>
-      <article dangerouslySetInnerHTML={{__html: marked(text)}}></article>
-    </div>
-  )
+export default class extends React.Component {
+  static async getInitialProps({res, query}) {
+    const { data } = await axios(`http://localhost:3001/${query.id}`);
+    if (data.code === 'ENOENT' ) {
+      res.statusCode = 404;
+      return {text: "404"};
+    }
+    return { text: data }
+  }
+
+  render() {
+    const { text } = this.props;
+    return (
+        <div className="container">
+        <nav><a>Home</a> <a>Export</a></nav>
+        <header></header>
+        <article dangerouslySetInnerHTML={{__html: marked(text)}}></article>
+        </div>
+    )
+  }
 }
---
+++ pages/post.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => {
+  return (
+      <div className="add-text">
+      <textarea></textarea>
+      <textarea></textarea>
+      </div>
+  );
+}
