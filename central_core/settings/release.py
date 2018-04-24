de6827ea023ebdb5126485e0a17801b54495be15
Mark Vasilkov <mvasilkov@gmail.com>
2018-04-24 14:09:03 +0300

    Django settings for DEBUG=False

--- central_core/settings/release.py
+++ central_core/settings/release.py
@@ -3,3 +3,8 @@
 from .base import *
 
 DEBUG = False
+
+ALLOWED_HOSTS = ['*']  # This is fine, since the application is internal
+
+SECRET_KEY = ('In the event that the weighted companion cube does speak, '
+              'the Enrichment Center urges you to disregard its advice.')
