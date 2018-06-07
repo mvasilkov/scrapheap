61fd45caf20da3d4675f2a154074d0705fd1f018
Mark Vasilkov <mvasilkov@gmail.com>
2018-06-07 17:23:05 +0300

    Re-link issues after refreshing the page contents

--- queueapp/static/queueapp/queueapp.js
+++ queueapp/static/queueapp/queueapp.js
@@ -1,14 +1,16 @@
 const JIRA_PREFIX = 'https://jira.infinidat.com/browse/'
 
-let issueLinks = document.querySelectorAll('a.issue:not([src])')
-issueLinks = Array.prototype.slice.call(issueLinks)
-issueLinks.forEach(a => {
-    a.href = JIRA_PREFIX + a.textContent
-})
+function autoLinkIssues() {
+    let issueLinks = document.querySelectorAll('a.issue:not([src])')
+    issueLinks = Array.prototype.slice.call(issueLinks)
+    issueLinks.forEach(a => {
+        a.href = JIRA_PREFIX + a.textContent
+    })
+}
 
-function autoreload(seconds) {
+function autoreload(seconds, done) {
     setInterval(function () {
-        $('.container').load(location.toString() + ' .container>article', pageDown)
+        $('.container').load(location.toString() + ' .container>article', done)
     }, seconds)
 }
 
