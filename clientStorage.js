73ffa5faf3465776ec3aaffde9672011ea181310
Mark Vasilkov <mvasilkov@gmail.com>
2018-07-01 15:42:11 +0300

    Build

--- clientStorage.js
+++
@@ -1,7 +0,0 @@
-define("clientStorage", ["require", "exports"], function (require, exports) {
-    "use strict";
-    Object.defineProperty(exports, "__esModule", { value: true });
-    class ClientStorage {
-    }
-    exports.ClientStorage = ClientStorage;
-});
--- clientStorage/clientStorage.ts
+++ clientStorage/clientStorage.ts
@@ -1,4 +1,9 @@
 import { Dropbox } from 'dropbox'
 
 export class ClientStorage {
+    db: Dropbox
+
+    constructor(accessToken: string) {
+        this.db = new Dropbox({ accessToken })
+    }
 }
