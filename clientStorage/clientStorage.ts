abfc830ae746a446dd0ccbcae156199635941857
Mark Vasilkov <mvasilkov@gmail.com>
2018-07-02 15:22:42 +0300

    saveAccessToken

--- clientStorage/clientStorage.ts
+++ clientStorage/clientStorage.ts
@@ -1,15 +1,38 @@
 import { Dropbox } from 'dropbox'
 
-interface ICSOptions {
+type QueryArgs = { [k: string]: string | undefined }
+
+function queryArgs(): QueryArgs {
+    const result: QueryArgs = {}
+
+    location.hash.replace(/^#/, '').split('&')
+        .map(a => a.split('='))
+        .forEach(([a, ...b]) => {
+            result[decodeURIComponent(a)] = decodeURIComponent(b.join('='))
+        })
+
+    return result
+}
+
+interface IOptions {
     accessToken?: string,
     clientId?: string,
+    saveAccessToken?: (accessToken: string) => void,
 }
 
 export class ClientStorage {
     readonly db: Dropbox
 
-    constructor(path: string, options: ICSOptions) {
-        const { accessToken, clientId } = options
+    constructor(path: string, options: IOptions) {
+        let { accessToken } = options
+        const { clientId, saveAccessToken } = options
+
+        if (accessToken == null) {
+            if (accessToken = queryArgs()['access_token']) {
+                if (saveAccessToken) saveAccessToken(accessToken)
+            }
+        }
+
         this.db = new Dropbox({ accessToken, clientId })
     }
 
