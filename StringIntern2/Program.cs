3f79c5e757e7db240c6ccb5c438f38730bd8b6d5
Mark Vasilkov <mvasilkov@gmail.com>
2019-07-27 21:02:40 +0300

    StringIntern2

---
+++ StringIntern2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace StringIntern2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            String a = "Hope is the first step on the road to disappointment.";
+            String b = "Hope is the first step on the road to disappointment.";
+
+            Console.WriteLine(string.Format("Strings are equal: {0}",
+                a == b));
+            Console.WriteLine(string.Format("Pointers are equal: {0}",
+                (Object)a == (Object)b));
+
+            String sb = new StringBuilder("Hope is the first step")
+                .Append(" on the road to disappointment.").ToString();
+
+            Console.WriteLine(string.Format("Strings are equal: {0}",
+                a == sb));
+            Console.WriteLine(string.Format("Pointers are equal: {0}",
+                (Object)a == (Object)sb));
+        }
+    }
+}
