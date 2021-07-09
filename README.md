3319ddd317190fe4091f5ea7a3c74d000489ada7
Mark Vasilkov <mvasilkov@gmail.com>
2021-07-09 22:48:00 +0300

    Database

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 Sneed's Overflow (Formerly Chuck's)
 ===
+
+Tech stack:
+
+- Node.js®
+- Express
+- [lowdb][lowdb]
+
+[lowdb]: https://github.com/typicode/lowdb
--- app.js
+++ app.js
@@ -1,13 +1,52 @@
+'use strict'
 const express = require('express')
-const app = express()
-const port = 3000
+const { ObjectId } = require('bson')
 
-app.use(express.static('public'))
+async function run() {
+    // Setting up the database
+    const lowdb = await import('lowdb') // Pure ESM package can choke on a bucket of cocks
+    const db = new lowdb.Low(new lowdb.JSONFile(__dirname + '/db.json'))
+    await db.read()
 
-app.get('/hello', (req, res) => {
-    res.send('hello world')
-})
+    if (!db.data) { // No database file on disk
+        db.data = {
+            questions: {},
+        }
+    }
 
-app.listen(port, () => {
-    console.log(`Sneed's Overflow app listening at http://localhost:${port}`)
-})
+    // Setting up the web server
+    const app = express()
+    const port = 3000
+
+    app.use(express.static('public'))
+    app.use(express.json())
+
+    app.get('/api/questions', (req, res) => {
+        res.json(db.data.questions)
+    })
+
+    app.post('/api/questions', (req, res) => {
+        const { title, text } = req.body
+        if (!title || !text) {
+            res.json('no')
+            return
+        }
+
+        const objectid = (new ObjectId).toHexString()
+        db.data.questions[objectid] = {
+            title,
+            text,
+        }
+        db.write()
+
+        res.json('ok')
+    })
+
+    app.listen(port, () => {
+        console.log(`Sneed's Overflow app listening at http://localhost:${port}`)
+    })
+}
+
+if (require.main === module) {
+    run()
+}
