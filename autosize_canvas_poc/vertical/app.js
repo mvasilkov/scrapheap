6aeb96983e8431d0ab92b1ea9a961150363d1bee
Mark Vasilkov <mvasilkov@gmail.com>
2022-05-22 22:59:33 +0300

    Test app for canvas autorotate

---
+++ autosize_canvas_poc/vertical/app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+import { CanvasHandle } from '../node_modules/natlib/canvas/CanvasHandle.js'
+import { AutoScaleWrapper } from '../node_modules/natlib/canvas/AutoScaleWrapper.js'
+import { AutoRotateWrapper } from '../node_modules/natlib/canvas/AutoRotateWrapper.js'
+
+const canvas = new CanvasHandle(document.querySelector('#canvas1'), 540, 960)
+const con = canvas.con
+
+con.fillStyle = '#101010'
+con.fillRect(0, 0, canvas.width, canvas.height)
+
+con.fillStyle = '#fbfbfb'
+con.fillRect(0.5 * canvas.width - 20, 40, 40, 40)
+
+// const autoscale = new AutoScaleWrapper(document.querySelector('#autoscale1'), 540, 960)
+const autoscale = new AutoRotateWrapper(document.querySelector('#autoscale1'), 540, 960)
+autoscale.addEventListeners()
