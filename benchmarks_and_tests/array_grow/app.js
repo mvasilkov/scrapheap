cda1bea1286415e8794453f7c46bee197cd1d458
Mark Vasilkov <mvasilkov@gmail.com>
2022-09-17 22:05:41 +0300

    heapsort

--- benchmarks_and_tests/array_grow/app.js
+++ benchmarks_and_tests/array_grow/app.js
@@ -1,13 +1,20 @@
-const Benchmark = require('benchmark')
+import assert from 'assert/strict'
 
-const { PriorityQueue } = require('./PriorityQueue.js')
-const { PriorityQueueNoGrow } = require('./PriorityQueueNoGrow.js')
+import Benchmark from 'benchmark'
+import { heapsort, PriorityQueue as NPriorityQueue } from 'natlib/collections/PriorityQueue.js'
+
+import { PriorityQueue } from './out/PriorityQueue.js'
+import { PriorityQueueNG } from './out/PriorityQueueNG.js'
 
 const list = Array.from({ length: 1000 }, function () {
   return Math.random()
 })
 
 const suite = new Benchmark.Suite
+
+let result1
+let result2
+let result3
 
 suite
   .add('grow', function () {
@@ -16,14 +23,28 @@ suite
       comparatorFn: (a, b) => a - b,
       initialValues: list.slice(),
     })
+    result1 = q.heapsort()
 
   })
   .add('no grow', function () {
 
-    const q = new PriorityQueueNoGrow({
+    const q = new PriorityQueueNG({
       comparatorFn: (a, b) => a - b,
       initialValues: list.slice(),
     })
+    result2 = q.heapsort()
+
+  })
+  .add('natlib', function () {
+
+    // const q = new NPriorityQueue(
+    //   (a, b) => a - b,
+    //   list.slice(),
+    // )
+    result3 = heapsort(
+      (a, b) => a - b,
+      list.slice(),
+    )
 
   })
   .on('cycle', function (event) {
@@ -33,3 +54,19 @@ suite
     console.log('Fastest is ' + this.filter('fastest').map('name'))
   })
   .run({ 'async': false })
+
+// Check the results
+
+assert(result1.length === 1000)
+assert(result2.length === 1000)
+assert(result3.length === 1000)
+
+list.sort((a, b) => a - b)
+
+for (let n = 0; n < 1000; ++n) {
+  assert(list[n] === result1[n])
+  assert(list[n] === result2[n])
+  assert(list[n] === result3[n])
+
+  if (n % 100 === 0) console.log(`Checked ${n}`)
+}
---
+++ benchmarks_and_tests/array_grow/out/PriorityQueue.js
@@ -0,0 +1,88 @@
+export class PriorityQueue {
+    constructor(params) {
+        this.values = [];
+        this.length = 0;
+        this.comparatorFn = params.comparatorFn;
+        if (params.initialValues) {
+            params.initialValues.forEach(value => this.insert(value));
+        }
+    }
+    insert(value) {
+        if (this.values.length <= this.length) {
+            this.values.length = Math.max(1, this.values.length * 2);
+        }
+        this.values[this.length++] = value;
+        this.bubbleUp();
+    }
+    remove() {
+        if (this.length === 0)
+            return null;
+        const node = this.values[0];
+        if (this.length === 1) {
+            this.length = 0;
+            this.values[0] = null;
+            return node;
+        }
+        this.values[0] = this.values[this.length - 1];
+        this.values[this.length - 1] = null;
+        this.length--;
+        this.bubbleDown();
+        return node;
+    }
+    heapsort() {
+        return Array.from({ length: this.length }, () => this.remove());
+    }
+    parent(nodeIndex) {
+        if (nodeIndex === 0)
+            return null;
+        return (nodeIndex - 1) >>> 1;
+    }
+    leftChild(nodeIndex) {
+        const child = (nodeIndex * 2) + 1;
+        if (child >= this.length)
+            return null;
+        return child;
+    }
+    rightChild(nodeIndex) {
+        const child = (nodeIndex * 2) + 2;
+        if (child >= this.length)
+            return null;
+        return child;
+    }
+    bubbleUp() {
+        let index = this.length - 1;
+        while (true) {
+            const parent = this.parent(index);
+            if (parent !== null && this.comparatorFn(this.values[index], this.values[parent]) < 0) {
+                const tmp = this.values[index];
+                this.values[index] = this.values[parent];
+                this.values[parent] = tmp;
+                index = parent;
+                continue;
+            }
+            return;
+        }
+    }
+    bubbleDown() {
+        let index = 0;
+        while (true) {
+            const left = this.leftChild(index);
+            const right = this.rightChild(index);
+            let swapCandidate = index;
+            if (left !== null && this.comparatorFn(this.values[swapCandidate], this.values[left]) > 0) {
+                swapCandidate = left;
+            }
+            if (right !== null && this.comparatorFn(this.values[swapCandidate], this.values[right]) > 0) {
+                swapCandidate = right;
+            }
+            if (swapCandidate !== index) {
+                const tmp = this.values[index];
+                this.values[index] = this.values[swapCandidate];
+                this.values[swapCandidate] = tmp;
+                index = swapCandidate;
+                continue;
+            }
+            return;
+        }
+    }
+}
